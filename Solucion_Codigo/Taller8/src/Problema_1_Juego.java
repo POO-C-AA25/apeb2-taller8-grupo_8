789ca63933eb4fb6a232022289a344773fae6c62
Stalinn99 <s.lennin@yahoo.com>
2025-07-04 10:29:21 -0500

    Inicio

---
+++ Solucion_Codigo/Taller8/src/Problema_1_Juego.java
@@ -0,0 +1,38 @@
+
+/**
+ * En un juego de rol, se desea implementar un sistema de combate en el que
+ * participen diferentes tipos de personajes: guerreros, magos y arqueros.
+ * Cada personaje tiene atributos y habilidades únicas, así como diferentes
+ * métodos de ataque y defensa.
+ *
+ * El objetivo del juego es enfrentar a los personajes en batallas y determinar
+ * el ganador en función de sus habilidades, estrategias y atributos.
+ * Los guerreros se destacan por su fuerza y habilidades cuerpo a cuerpo,
+ * los magos por sus hechizos y poderes mágicos, y los arqueros por su precisión
+ * y habilidades a distancia.
+ *
+ * El sistema debe permitir crear nuevos personajes de cada tipo, asignarles
+ * atributos iniciales, como puntos de vida y nivel de experiencia,
+ * y permitirles subir de nivel a medida que ganan batallas.
+ * Además, se debe implementar un algoritmo de combate que evalúe las
+ * habilidades de cada personaje y determine el resultado de la batalla.
+ *
+ * Utilizando programación orientada a objetos, herencia y polimorfismo,
+ * implementa el sistema de combate y las clases necesarias para representar
+ * a los diferentes tipos de personajes. Asegúrate de que cada tipo de personaje
+ * tenga sus propias habilidades y métodos de ataque y defensa, y que puedan
+ * interactuar entre sí en las batallas.
+ *
+ * Note
+ *
+ * Para solucionar lo anterior se debe generar lo siguiente:
+ *
+ * Un diagrama exclusivo que involucren las funcionalidades principales del juego.
+ * Una solución en lenguaje de programación Java. Usar Polimorfismo en la solución.
+ * Clase de prueba/ejecutor, que demuestre la funcionalidad del juego.
+ *
+ * @author Daniel Irene
+ */
+public class Problema_1_Juego {
+
+}
